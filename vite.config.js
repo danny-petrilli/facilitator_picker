df00d8e3b7f13fdd32d5a23ed2c27728315167dc
Danny Petrilli <danny.petrilli@merkle.com>
2025-05-16 15:32:35 -0400

    fix: updates vite config for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/facilitator_picker/',  // Replace with your repository name
+  base: '/facilitator_picker/', // Must match your repository name exactly
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
